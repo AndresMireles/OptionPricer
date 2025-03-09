b221ba4ba1086d65685a49d782ef74664ef9af1c
Andres Mireles <andresmireles2001@gmail.com>
2025-03-09 17:33:25 +0100

    Minor changes

--- optionpricer.cpp
+++ optionpricer.cpp
@@ -221,7 +221,7 @@ double OptionPricer::computePricePDE(double S0, double maturity, double volatili
     initializeConditions(maturity, deltaR);
     performCalculations(volatility, deltaR);
 
-    // Return the option price at S0 (we need to check where is it since the grid can be not symmetric)
+    // Return the option price at [T0,S0] (we need to check where is it since the grid can be not symmetric)
     auto it = std::min_element(spotPrices_.begin(), spotPrices_.end(),
         [&](double a, double b) { return std::abs(a - S0) < std::abs(b - S0); });
     int index = std::distance(spotPrices_.begin(), it);
